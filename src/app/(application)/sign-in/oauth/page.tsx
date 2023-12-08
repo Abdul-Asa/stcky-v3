eec1c2f003f0a43cbb0c34a74faac010a39d96a0
Your Name <bigtulahi1@gmaiil.com>
2023-12-08 04:07:07 +0000

    AI got involved

--- src/app/(application)/sign-in/oauth/page.tsx
+++ src/app/(application)/sign-in/oauth/page.tsx
@@ -18,35 +18,34 @@ export default function OAuth() {
   }, 10000);
 
   useEffect(() => {
-    console.log("Run on mount");
-    if (!magic) return console.log("Magic not initialized");
-    if (
-      !searchParams.get("magic_credential") &&
-      !searchParams.get("magic_oauth_request_id")
-    ) {
-      setMessage("No Credentials found");
-      return;
-    }
-    console.log("here 1");
+    const fetchRedirectResult = async () => {
+      if (!magic) return console.log("Magic not initialized");
+      if (
+        !searchParams.get("magic_credential") &&
+        !searchParams.get("magic_oauth_request_id")
+      ) {
+        setMessage("No Credentials found");
+      }
+      console.log("here 1");
 
-    magic.oauth
-      .getRedirectResult()
-      .then(async (res) => {
+      try {
+        const res = await magic.oauth.getRedirectResult();
         console.log("here 2" + res);
         const token = await setSessionToken(res.magic.idToken);
         console.log("here 3" + token);
         if (token.error) {
           showToast({ message: token.error, type: "error" });
-          setMessage("Youre ready to go!");
+          setMessage("You're ready to go!");
         } else router.push("/space");
-      })
-      .catch((e) => {
+      } catch (e) {
         console.log("here 3.5" + e);
         console.log(e);
-      });
-    console.log("here 4");
-  }, [router, searchParams]);
+      }
+      console.log("here 4");
+    };
 
+    fetchRedirectResult();
+  }, [router, searchParams]);
   return (
     <div className="flex items-center justify-center min-h-screen ">
       <div className="max-w-sm p-6 space-y-6 border rounded-lg shadow-lg dark:border-gray-700">
